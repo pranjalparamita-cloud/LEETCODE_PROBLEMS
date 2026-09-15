1992247c592ebdb246b1a8c0d741b0d1b484113c
PRANJAL PARAMITA SAHU <pranjalparamita@gmail.com>
2026-09-15 11:31:48 +0530

    Time: 0 ms (100%), Space: 43.3 MB (88.53%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                zero++;
+            }
+            else if (nums[i] == 1) {
+                one++;
+            }
+            else {
+                two++;
+            }
+        }
+
+        int i = 0;
+
+        while (zero > 0) {
+            nums[i] = 0;
+            i++;
+            zero--;
+        }
+
+        while (one > 0) {
+            nums[i] = 1;
+            i++;
+            one--;
+        }
+
+        while (two > 0) {
+            nums[i] = 2;
+            i++;
+            two--;
+        }
+    }
+}
