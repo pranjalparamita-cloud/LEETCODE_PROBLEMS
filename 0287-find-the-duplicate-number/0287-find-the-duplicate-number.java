4c19671cafe55091b4e564278e6ba6719609fe5b
PRANJAL PARAMITA SAHU <pranjalparamita@gmail.com>
2026-09-16 22:00:27 +0530

    Time: 46 ms (5.89%), Space: 79.5 MB (89.83%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            if(nums[left]==nums[left+1]){
+                return nums[left];
+            }
+
+        left++;
+        }
+        return -1;
+    }
+}
