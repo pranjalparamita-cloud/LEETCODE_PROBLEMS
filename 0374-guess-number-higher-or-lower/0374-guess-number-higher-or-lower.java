8bc914abc78f2beb42e2cc5bf1400e4b23ca7120
PRANJAL PARAMITA SAHU <pranjalparamita@gmail.com>
2026-09-16 21:34:47 +0530

    Time: 0 ms (100%), Space: 41.8 MB (86.05%) - LeetHub

---
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
@@ -0,0 +1,20 @@
+public class Solution extends GuessGame {
+
+    public int guessNumber(int n) {
+        int l = 1;
+        int r = n;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (guess(mid) == 0) {
+                return mid;
+            }
+            else if (guess(mid) == -1) {
+                r = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
