2bc12b7651dab6b40f93cf13ebd9fdb113d85fd2
PRANJAL PARAMITA SAHU <pranjalparamita@gmail.com>
2026-09-23 15:52:10 +0530

    Time: 0 ms (100%), Space: 43.6 MB (42.81%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,7 +3,7 @@ class Solution {
         int l=0;
         int r=0;
         while(r<nums.length){
-            if (nums[r]!=val){
+            if(nums[r]!=val){
                 nums[l]=nums[r];
                 l++;
             }
