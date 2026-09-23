079940f6d5f61cf9a82e6410781da0597a1d3b3f
PRANJAL PARAMITA SAHU <pranjalparamita@gmail.com>
2026-09-23 17:25:47 +0530

    Time: 1 ms (99.94%), Space: 94.5 MB (51.67%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -4,15 +4,15 @@ class Solution {
         int r=1;
         int profit=0;
         while(r<prices.length){
-                if (prices[l]<prices[r]){
-                    profit=Math.max(profit,prices[r]-prices[l]);
-                }
-                else{
-                    l=r;
-                }
-                r++;
-            
+            if(prices[l]<prices[r]){
+                profit=Math.max(profit,prices[r]-prices[l]);
+                
+            }
+            else{
+                l=r;
+            }
+            r++;
         }
-        return profit;  
+       return profit;
     }
-}
+} 
