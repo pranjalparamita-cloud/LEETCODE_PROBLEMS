8b31a78806d4d2c142422cf821f9c0eb68c3be3d
PRANJAL PARAMITA SAHU <pranjalparamita@gmail.com>
2026-09-23 15:44:19 +0530

    Time: 1 ms (77.48%), Space: 47 MB (9.14%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,14 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int l=0;
-        int r=1;
-        while(r<nums.length){
-            if (nums[l]!=nums[r]){      
-                l++;
-                nums[l]=nums[r];
-            }
-            r++;
+       int l=0;
+       int r=1;
+       while (r<nums.length){
+        if(nums[l]!=nums[r]){
+            l++;
+            nums[l]=nums[r];
         }
-        return l+1;
+        r++;
+       }
+       return l+1;
     }
 }
